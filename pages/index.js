e8402217ddec58b1448bd10a8b70473f16bd832d
Shivam Singh <shivam.singh102201@gmail.com>
2023-12-25 21:02:29 +0530

    Commit

---
+++ pages/index.js
@@ -0,0 +1,182 @@
+import CommentedImage from '../components/Commented Image'
+import CornyQuestion from '../components/Corny Question'
+import RandomButton from '../components/Random Button'
+
+import { useRef, useState } from 'react'
+
+import {
+  Button,
+  Code,
+  useDisclosure,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from '@chakra-ui/react'
+
+export default function Home() 
+{
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = useRef()
+  const [confirmed, setConfirmation] = useState(false)
+
+  return (
+    <>
+    
+      <div className="flex flex-row justify-center items-center w-full h-full bg-blue-200 p-10 drop-shadow-xl">
+        <div className="w-2/3 h-64 bg-gradient-to-r from-blue-300 to-blue-200 drop-shadow-xl rounded-full text-center transition-all duration-300">
+          <h1 className="p-20 tracking-wide text-8xl font-bold text-white uppercase drop-shadow-lg">Hi Beautiful</h1>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center justify-evenly w-full h-96 bg-blue-50">
+        <CommentedImage src='/san1.jpg' text="wow so pretty"/>
+        <CommentedImage src='/san2.jpg' text="omg we&apos;re so cute"/>
+        <CommentedImage src='/san3.jpg' text="we should kiss?"/>
+      </div>
+
+      <div className="text-center w-full h-64 bg-blue-200 drop-shadow-xl">
+        <h1 className="p-20 tracking-wide text-6xl font-bold text-blue-400 uppercase drop-shadow-xl">So I heard...</h1>
+      </div>
+
+      <div className="flex flex-col items-center text-center w-full h-96 bg-blue-50 p-10">
+        <div className="flex flex-row items-center text-center w-full h-5/6 bg-blue-50">
+          <h1 className='w-1/3 text-blue-300 font-bold text-6xl'>You Need</h1>
+          <img src='https://sweezy-cursors.com/wp-content/uploads/cursor/milk-amp-mocha-bear-love/milk-mocha-bear-love-custom-cursor.png' width={600}/>
+          <h1 className='w-1/3 text-blue-300 font-bold text-6xl'>Some</h1>
+        </div>
+        <h1 className="w-full h-1/4 text-blue-300 font-bold text-6xl uppercase">LOVE?</h1>
+      </div>
+
+      <div className="text-center w-full h-64 bg-blue-200 drop-shadow-xl">
+        <h1 className="p-20 tracking-wide text-6xl font-bold text-blue-400 uppercase drop-shadow-xl">So...</h1>
+      </div>
+
+      <CornyQuestion 
+        question="They say nothing lasts forever, so will you be my nothing?"
+        nonactive="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Nuclear_symbol.svg/1024px-Nuclear_symbol.svg.png"
+        active="https://hips.hearstapps.com/hmg-prod/images/best-love-quotes-of-all-time-envelope-with-hearts-1611079864.jpg?crop=0.662xw:0.993xh;0.00163xw,0.00733xh&resize=1200:*"
+        primary
+        left
+      />
+
+      <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-blue-200 p-5">
+        <h1 className='w-2/3 text-blue-400 font-bold text-6xl'>Would you</h1>
+      </div>
+
+      <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-blue-50 p-5">
+        <h1 className='w-2/3 text-blue-300 font-bold text-6xl'>Uh</h1>
+      </div>
+
+      <CornyQuestion 
+        question="Would you be the butt to my Red Guy?"
+        nonactive="https://loudbooru.com/_images/a1c2fa54153d30da1045a967fe8fd8a9/9228%20-%20character%3Alincoln_loud%20character%3Athe_red_guy%20cow_and_chicken%20crossover%20devil%20dialogue%20satan.png"
+        active="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/860bb581-65ee-4112-854f-1255c5579143/dffb3t8-d1a0991f-db5c-49ff-a7f2-2bc8d3278004.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg2MGJiNTgxLTY1ZWUtNDExMi04NTRmLTEyNTVjNTU3OTE0M1wvZGZmYjN0OC1kMWEwOTkxZi1kYjVjLTQ5ZmYtYTdmMi0yYmM4ZDMyNzgwMDQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.F2pBVw7v1HfA3zLlC5V5lc1xberJTFDwLT_GoKlT7Vw"
+        secondary
+        right
+      />
+
+      <CornyQuestion 
+        question="Would you be the smut to my Among Us?"
+        nonactive="https://assets.stickpng.com/images/61d183263a856e0004c6334a.png"
+        active="https://pbs.twimg.com/media/E2ieswGVgAYKWt3.jpg"
+        primary
+        left
+      />
+
+      <CornyQuestion 
+        question="Would you be the banana to my Minion?"
+        nonactive="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0a76b087-d923-4acf-85c5-118e6b81c7a0/d70lfsz-0a567aaf-a3ec-4cca-ac87-3820e4727e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBhNzZiMDg3LWQ5MjMtNGFjZi04NWM1LTExOGU2YjgxYzdhMFwvZDcwbGZzei0wYTU2N2FhZi1hM2VjLTRjY2EtYWM4Ny0zODIwZTQ3MjdlOGYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PZQsxdrOyzYABwsLATW4K94fX8RQBzUIzLrR-l8BDPU"
+        active="https://i.redd.it/v0ep5v1vbl331.png"
+        secondary
+        right
+      />
+
+      <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-blue-50 px-5 pt-5">
+        <h1 className='w-2/3 text-blue-400 font-bold text-6xl'>If I&apos;m C would you be my C++ for life?</h1>
+      </div>
+
+      <div className="flex flex-row justify-around items-start text-center w-full h-36 bg-blue-50 pb-5">
+        <RandomButton 
+          text="No"
+          minHeight={ 4600 }
+          maxHeight={ 5000 }
+        />
+
+        <button 
+          className="w-36 bg-blue-400 text-white text-2xl p-5 rounded-2xl"
+          onClick={ onOpen }
+        >
+          YES
+        </button>
+
+        <AlertDialog
+          isOpen={ isOpen }
+          leastDestructiveRef={ cancelRef }
+          onClose={ onClose } 
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+
+              <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                OMG.
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                NO WAY SHE JUST SAID YES?
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button 
+                  colorScheme='blue' 
+                  ref={ cancelRef } 
+                  onClick={
+                    () => 
+                    {
+                      onClose();
+                      setConfirmation(true);
+                    }
+                  }
+                >
+                  Confirm
+                </Button>
+              </AlertDialogFooter>
+
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </div>
+
+      {
+        confirmed ? 
+        <div className="flex flex-col justify-around items-center text-center w-full h-[58em] bg-blue-200 p-5">
+          <h1 className='w-2/3 text-blue-400 font-bold text-6xl'>Confirmation Receipt</h1>
+          <div className="flex flex-col items-center gap-5 text-center w-3/4 h-5/6 bg-white p-5 rounded-3xl shadow-xl">
+            <img className="h-24" src="https://cdn.pixabay.com/photo/2018/02/19/20/28/abstract-3166168__480.png" alt=""/>
+            <h1 className='w-2/3 text-black font-bold text-4xl underline'>
+              Thank You For Your Confirmation!
+            </h1>
+            <h2 className='w-2/3 text-black opacity-75 font-bold italic text-2xl'>
+              Name of Partner
+            </h2>
+            <h3 className='w-2/3 text-black opacity-90 font-bold text-2xl'>
+              Shivam Singh
+            </h3>
+            <h2 className='w-2/3 text-black opacity-75 font-bold italic text-2xl'>
+              Location
+            </h2>
+            <h3 className='w-2/3 text-black opacity-90 font-bold text-2xl'>
+              Bangalore, India
+            </h3>
+            <img className="h-24" src="https://cdn.pixabay.com/photo/2018/02/19/20/28/abstract-3166168__480.png" alt=""/>
+          </div>
+        </div>
+        : 
+        <></>
+      }
+    
+    </>
+  )
+}
